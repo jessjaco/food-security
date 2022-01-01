324285d0ffc48a084ba438367acc1a4734e1ff5f
Jesse Anderson <jesse.anderson@gmail.com>
2022-01-01 12:22:14 -0700

    6b

--- src/model.R
+++ src/model.R
@@ -5,11 +5,17 @@ source("src/utils.R")
 
 d <- read_csv("data/training_data.csv")
 test_d <- read_csv("data/test_data.csv")
+model_id <- "6b"
 
 params <- list(
   max_depth = 5, min_child_weight = 1, gamma = 0, subsample = 0.8,
-  colsample_bytree = 0.8, eta = 0.2, nthread = 7,
+  colsample_bytree = 0.8, eta = 0.1, nthread = 7,
   objective = "multi:softprob", num_class = length(unique(d$crop_id))
 )
 
-results <- run_run(d, test_d, params, "9")
+results <- run_run(d, test_d, params, model_id)
+
+training_predictions <- make_predictions(results$model, d)
+read_sf("data/training_labels.geojson") %>%
+  left_join(training_predictions) %>%
+  write_sf(glue("data/submissions/{model_id}_training_predictions.geojson"))
